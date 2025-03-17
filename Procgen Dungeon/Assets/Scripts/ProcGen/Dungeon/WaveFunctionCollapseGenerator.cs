3531938ef713a24bd1aba04c9821e6d991d7fa76
rjoverbeck <r.j.overbeck.2@gmail.com>
2025-03-17 14:00:51 -0500

    Add script for WFC to project. Tweak WFC section intro for paper

---
+++ Procgen Dungeon/Assets/Scripts/ProcGen/Dungeon/WaveFunctionCollapseGenerator.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveFunctionCollapseGenerator
+{
+    public static Queue<Vector2Int> Generate()
+    {
+        return null;
+    }
+
+    // 
+
+}
