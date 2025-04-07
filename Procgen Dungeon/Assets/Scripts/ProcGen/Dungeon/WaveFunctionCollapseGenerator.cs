ea5ea6ac70f6dd353eb79aebff03884978af0292
rjoverbeck <r.j.overbeck.2@gmail.com>
2025-04-07 10:36:35 -0500

    Paper - fix references spacing, add title. Project - add logic for mapping out viable tiles for every tile's edge

--- Procgen Dungeon/Assets/Scripts/ProcGen/Dungeon/WaveFunctionCollapseGenerator.cs
+++ Procgen Dungeon/Assets/Scripts/ProcGen/Dungeon/WaveFunctionCollapseGenerator.cs
@@ -7,17 +7,19 @@ public class WaveFunctionCollapseGenerator
     {
         Color[][][] tilesEdgesColors = GetTilesEdgesColors(tiles);
 
+        int[][][] tilesEdgesViableTiles = GetTilesEdgesViableTiles(tilesEdgesColors);
+
         // temporary logging
-        for (int i = 0; i < tilesEdgesColors.Length; i++)
+        for (int i = 0; i < tilesEdgesViableTiles.Length; i++)
         {
             string logMsgTile0 = $"Tile {i}:" +
-                                 $"\nBottom: {string.Join(", ", tilesEdgesColors[i][0])}";
+                                 $"\nBottom: {string.Join(", ", tilesEdgesViableTiles[i][0])}";
             string logMsgTile1 = $"Tile {i}:" +
-                                 $"\nTop: {string.Join(", ", tilesEdgesColors[i][1])}";
+                                 $"\nTop: {string.Join(", ", tilesEdgesViableTiles[i][1])}";
             string logMsgTile2 = $"Tile {i}:" +
-                                 $"\nLeft: {string.Join(", ", tilesEdgesColors[i][2])}";
+                                 $"\nLeft: {string.Join(", ", tilesEdgesViableTiles[i][2])}";
             string logMsgTile3 = $"Tile {i}:" +
-                                 $"\nRight: {string.Join(", ", tilesEdgesColors[i][3])}";
+                                 $"\nRight: {string.Join(", ", tilesEdgesViableTiles[i][3])}";
             Debug.Log(logMsgTile0);
             Debug.Log(logMsgTile1);
             Debug.Log(logMsgTile2);
@@ -27,6 +29,7 @@ public class WaveFunctionCollapseGenerator
         return null;
     }
 
+    // Get the color of every tile's edge pixels
     private static Color[][][] GetTilesEdgesColors(List<Texture2D> tiles)
     {
         Color[][][] tilesEdgesColors = new Color[tiles.Count][][];
@@ -37,30 +40,93 @@ public class WaveFunctionCollapseGenerator
             int width = tile.width;
             int height = tile.height;
 
-            Color[] topBorder = new Color[width];
-            Color[] bottomBorder = new Color[width];
-            Color[] leftBorder = new Color[height];
-            Color[] rightBorder = new Color[height];
+            Color[] bottom = new Color[width];
+            Color[] top = new Color[width];
+            Color[] left = new Color[height];
+            Color[] right = new Color[height];
 
             for (int x = 0; x < width; x++)
             {
-                bottomBorder[x] = tile.GetPixel(x, 0);
-                topBorder[x] = tile.GetPixel(x, height - 1);
+                bottom[x] = tile.GetPixel(x, 0);
+                top[x] = tile.GetPixel(x, height - 1);
             }
 
             for (int y = 0; y < height; y++)
             {
-                leftBorder[y] = tile.GetPixel(0, y);
-                rightBorder[y] = tile.GetPixel(width - 1, y);
+                left[y] = tile.GetPixel(0, y);
+                right[y] = tile.GetPixel(width - 1, y);
             }
 
             tilesEdgesColors[i] = new Color[4][];
-            tilesEdgesColors[i][0] = bottomBorder;
-            tilesEdgesColors[i][1] = topBorder;
-            tilesEdgesColors[i][2] = leftBorder;
-            tilesEdgesColors[i][3] = rightBorder;
+            tilesEdgesColors[i][0] = bottom;
+            tilesEdgesColors[i][1] = top;
+            tilesEdgesColors[i][2] = left;
+            tilesEdgesColors[i][3] = right;
         }
 
         return tilesEdgesColors;
+    }
+
+    // Get the index of every viable tile (tiles whose complementary edges e.g. right-left have the same colors for each pixel)
+    // for each tile's edge
+    private static int[][][] GetTilesEdgesViableTiles(Color[][][] tilesEdgesColors)
+    {
+        int tileCount = tilesEdgesColors.Length;
+        int[][][] tilesEdgesViableTiles = new int[tileCount][][];
+
+        for (int i = 0; i < tileCount; i++)
+        {
+            tilesEdgesViableTiles[i] = new int[4][];
+            for (int edge = 0; edge < 4; edge++)
+            {
+                List<int> viableTiles = new List<int>();
+                int complementaryEdge = GetComplementaryEdge(edge);
+
+                for (int j = 0; j < tileCount; j++)
+                {
+                    if (EdgesMatch(tilesEdgesColors[i][edge], tilesEdgesColors[j][complementaryEdge]))
+                    {
+                        viableTiles.Add(j);
+                    }
+                }
+
+                tilesEdgesViableTiles[i][edge] = viableTiles.ToArray();
+            }
+        }
+
+        return tilesEdgesViableTiles;
+    }
+
+    // Return the complementary edge index
+    private static int GetComplementaryEdge(int edge)
+    {
+        // 0=bottom, 1=top, 2=left, 3=right
+        switch (edge)
+        {
+            case 0: return 1;
+            case 1: return 0;
+            case 2: return 3;
+            case 3: return 2;
+            default: return -1;
+        }
+    }
+
+    // Return true if edge colors match (pixel for pixel)
+    private static bool EdgesMatch(Color[] edge1, Color[] edge2)
+    {
+        if (edge1.Length != edge2.Length)
+        {
+            return false;
+        }
+
+        for (int pixel = 0; pixel < edge1.Length; pixel++)
+        {
+            if (!edge1[pixel].Equals(edge2[pixel]))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
